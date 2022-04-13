50bbdfc3da6491b039e8e62469f0e0e8ec7fed6b
basta <jsembasta@gmail.com>
2022-04-13 12:43:20 +0200

    merging

--- henl/choochoo_app/views.py
+++ henl/choochoo_app/views.py
@@ -5,10 +5,17 @@ from datetime import datetime
 class LoadingView(TemplateView):
     template_name = "loading/loading.html"
 
+    def dispatch(self, request, *args, **kwargs):
+        print("AWDDDDDDDDDDD")
+
     def get_context_data(self, **kwargs):
         context = super(LoadingView, self).get_context_data(**kwargs)  # mostly useless
         data = {
-            "id": {"time": datetime.now(), "materials": [("asdsf", 20), ("465435d", 155), ("4354as", 217)]} for _ in range(10)
+            "id": {
+                "time": datetime.now(),
+                "materials": [("asdsf", 20), ("465435d", 155), ("4354as", 217)],
+            }
+            for _ in range(10)
         }
         context["data"] = data
 
