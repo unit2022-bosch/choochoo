8b0eaec97f5fc00d9c64ddf50748f273d19afffe
basta <jsembasta@gmail.com>
2022-04-13 11:25:49 +0200

    ex view

--- henl/choochoo_app/views.py
+++ henl/choochoo_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class LoadingView(TemplateView):
+    template_name = "loading/loading.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(LoadingView, self).get_context_data(**kwargs)  # mostly useless
+        context["bla"] = "to co chci"
