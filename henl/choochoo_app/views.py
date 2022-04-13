b59fb9b093591a32c281c5d978c6591862fe9aa2
basta <jsembasta@gmail.com>
2022-04-13 13:23:46 +0200

    model updates

--- henl/choochoo_app/views.py
+++ henl/choochoo_app/views.py
@@ -2,19 +2,26 @@ from datetime import datetime
 
 from django.views.generic import TemplateView
 
+from choochoo_app.models import Train
+
 
 class LoadingView(TemplateView):
     template_name = "loading/loading.html"
 
-    def dispatch(self, request, *args, **kwargs):
-        print("AWDDDDDDDDDDD")
-
     def get_context_data(self, **kwargs):
         context = super(LoadingView, self).get_context_data(**kwargs)  # mostly useless
         data = {
-            "id": {"time": datetime.now(), "materials": [("asdsf", 20), ("465435d", 155), ("4354as", 217)]} for _ in range(10)
+            "id": {
+                "time": datetime.now(),
+                "materials": [("asdsf", 20), ("465435d", 155), ("4354as", 217)],
+            }
+            for _ in range(10)
         }
         context["data"] = data
+        trains_to_load = Train.trains_to_be_loaded()
+        data = {}
+        for train in trains_to_load:
+            data[train.human_id] = train.get_orders()
 
         return context
 
@@ -24,6 +31,5 @@ class StationView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(StationView, self).get_context_data(**kwargs)  # mostly useless
-        context["bla"] = "to co chci"
 
         return context
