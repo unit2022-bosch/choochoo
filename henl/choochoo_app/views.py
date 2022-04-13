aef6de5af46fe0d5c96aeae4afe15e4655944bee
Adam Blažek <xigoi@disroot.org>
2022-04-13 12:28:44 +0200

    Template-based loading

--- henl/choochoo_app/views.py
+++ henl/choochoo_app/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import TemplateView
 from datetime import datetime
+
+from django.views.generic import TemplateView
 
 
 class LoadingView(TemplateView):
@@ -8,7 +9,11 @@ class LoadingView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(LoadingView, self).get_context_data(**kwargs)  # mostly useless
         data = {
-            "id": {"time": datetime.now(), "materials": [("asdsf", 20), ("465435d", 155), ("4354as", 217)]} for _ in range(10)
+            str(i): {
+                "time": datetime.now(),
+                "materials": [("asdsf", 20), ("465435d", 155), ("4354as", 217)],
+            }
+            for i in range(10)
         }
         context["data"] = data
 
