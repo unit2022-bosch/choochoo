38066849197fa5a0727552fb0b02012a7066112b
basta <jsembasta@gmail.com>
2022-04-13 11:37:35 +0200

    ex View"

--- henl/henl/urls.py
+++ henl/henl/urls.py
@@ -16,4 +16,4 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), include("choochoo_app.urls")]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("choochoo_app.urls"))]
