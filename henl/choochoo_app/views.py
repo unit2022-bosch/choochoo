9dfd808c0bb7580d386fc15847e99143da3b1e45
Your Name <you@example.com>
2022-04-13 12:12:00 +0200

    Mock data for loading page fic.

--- henl/choochoo_app/views.py
+++ henl/choochoo_app/views.py
@@ -8,7 +8,7 @@ class LoadingView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(LoadingView, self).get_context_data(**kwargs)  # mostly useless
         data = {
-            "id": {"time": datetime.now(), "materials": ["asdsf", "465435d", "4354as"]} for _ in range(10)
+            "id": {"time": datetime.now(), "materials": [("asdsf", 20), ("465435d", 155), ("4354as", 217)]} for _ in range(10)
         }
         context["data"] = data
 
